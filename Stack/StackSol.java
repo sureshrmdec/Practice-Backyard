a924e7154a74be96a9a65cc6c5bbc352a2ec6711
vtiwari227 <srmistvipin@yahoo.co.in>
2017-02-08 21:47:30 -0800

    Code: next greater element for array using stack

--- Stack/StackSol.java
+++ Stack/StackSol.java
@@ -1,4 +1,7 @@
 import java.lang.StringBuilder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class StackSol {
@@ -20,8 +23,76 @@ public class StackSol {
         }
         return sb.toString();
     }
+
+    /*
+    *Question :check for balanced parantehesis in an expression
+    *Approach : use stack to insert all opening parantehesis and for a closing
+    * parantehesis check if top of the stack you find corresponding match,
+    * if not then return false
+    *Time complexity : O(n) to traverse the string expression and O(n) space
+    * complexity for stack
+    */
+    public boolean isValidExpression(String input) {
+        Stack<Character> stack = new Stack<Character>();
+        char[] charArray = input.toCharArray();
+        for(char c : charArray) {
+            if (c == '{' || c == '(' || c == '[') stack.push(Character.valueOf(c));
+            if (c == '}' && stack.peek() == '{') stack.pop();
+            if (c == ')' && stack.peek() == '(') stack.pop();
+            if (c == ']' && stack.peek() == '[') stack.pop();
+        }
+        return stack.isEmpty();
+    }
+
+    public void intNextGreaterElement(int[] input) {
+        int size = input.length;
+
+        for(int i = 0; i < size; i++) {
+            int next = -1;
+            for(int j = i+1; j < size; j++) {
+                if(input[j] > input[i]) {
+                    next = input[j];
+                    break;
+                }
+            }
+            System.out.println(input[i]+"--->"+ next);
+        }
+    }
+
+    public void intNextGreaterElementStack(ArrayList<Integer> input) {
+        int size = input.length;
+        int next;
+        int element;
+        Stack<Integer> stack = new Stack<Integer>();
+        Map<Integer, Integer> valueMap = new HashMap<>();
+        stack.push(Integer.valueOf(input[0]));
+        for(int i = 1; i < size; i++) {
+            next = input[i];
+            if(!stack.isEmpty()) {
+                element = stack.pop();
+                while(element < next) {
+                    valueMap.put(element, next);
+                    if(stack.isEmpty()) break;
+                    element = stack.pop();
+                }
+                if (element > next) {
+                    stack.push(element);
+                }
+            }
+            stack.push(next);
+            while (!stack.isEmpty()) {
+                element = stack.pop();
+                valueMap.put(element, -1);
+            }
+        }
+        for(int i  = 0; i < size; i++) {
+            System.out.println(valueMap.get(i));
+        }
+    }
     public static void main(String[] args) {
         StackSol objStackSol = new StackSol();
-        System.out.println(objStackSol.reverseString("GeeksQuiz"));
+        int[] in = {4, 5, 2, 25};
+        ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(in)) ;
+        objStackSol.intNextGreaterElementStack(input);
     }
 }
