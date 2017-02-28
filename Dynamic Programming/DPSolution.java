d03de3ad09c1461edb4aac28a07b2fd3dd330b1a
vtiwari227 <srmistvipin@yahoo.co.in>
2017-02-27 18:10:25 -0800

    Code: minimum cost path using DP

--- Dynamic Programming/DPSolution.java
+++ Dynamic Programming/DPSolution.java
@@ -198,6 +198,31 @@ public class DPSolution {
      return result[s.length()];
     }
 
+    /**
+    *Problem : Minimum Path sum in m * n grid
+    *Link :https://leetcode.com/problems/minimum-path-sum/
+    *Time Complexity : O(n ^2) ; space Complexity : O(n^2)
+    */
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;// row
+       int n = grid[0].length; // column
+    	for (int i = 0; i < m; i++) {
+    		for (int j = 0; j < n; j++) {
+    			if (i == 0 && j != 0) {
+    				grid[i][j] = grid[i][j] + grid[i][j - 1];
+    			} else if (i != 0 && j == 0) {
+    				grid[i][j] = grid[i][j] + grid[i - 1][j];
+    			} else if (i == 0 && j == 0) {
+    				grid[i][j] = grid[i][j];
+    			} else {
+    				grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j])
+    						+ grid[i][j];
+    			}
+    		}
+    	}
+	return grid[m - 1][n - 1];
+    }
+
     public static void main(String[] args) {
         DPSolution objDP = new DPSolution();
         int arr[] = new int[]{2,3,1,1,4};
