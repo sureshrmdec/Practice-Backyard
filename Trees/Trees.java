6e8118829bffd7f51eb66f1dfe9b80ae408275c6
vtiwari227 <srmistvipin@yahoo.co.in>
2017-03-04 20:10:14 -0800

    Code: find frequent Tree Sum

--- Trees/Trees.java
+++ Trees/Trees.java
@@ -299,6 +299,49 @@ public class Trees {
         return result;
     }
 
+    /**
+    *Problem : find frequency sum of all subtrees in tree
+    *Link: https://leetcode.com/problems/most-frequent-subtree-sum/
+    */
+    class findFrequentTreeSum {
+    Map<Integer, Integer> sumToCount;
+    int maxCount;
+
+    public int[] findFrequentTreeSum(TreeNode root) {
+        maxCount = 0;
+        sumToCount = new HashMap<Integer, Integer>();
+
+        postOrder(root);
+
+        List<Integer> res = new ArrayList<>();
+        for (int key : sumToCount.keySet()) {
+            if (sumToCount.get(key) == maxCount) {
+                res.add(key);
+            }
+        }
+
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+
+        int sum = left + right + root.val;
+        int count = sumToCount.getOrDefault(sum, 0) + 1;
+        sumToCount.put(sum, count);
+
+        maxCount = Math.max(maxCount, count);
+
+        return sum;
+    }
+}
     public List<List<Integer>> inorderDFSUtil(List<List<Integer>> res, TreeNode root, int level) {
         if(root == null) return;
         if(level >= levelResult) {
@@ -329,7 +372,7 @@ public class Trees {
         }
        return sum;
     }
-    
+
     public static void main(String[] args) {
         Trees tree = new Trees();
         tree.root = new TreeNode(5);
