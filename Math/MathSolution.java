3c81f06f7eb2b7c4e149ac464baacf56c0edb8a4
vtiwari227 <srmistvipin@yahoo.co.in>
2017-03-07 09:07:17 -0800

    code : Pascal triangle for given rowNumber

--- Math/MathSolution.java
+++ Math/MathSolution.java
@@ -22,6 +22,48 @@ public class MathSolution {
         }
         return lookupTable[n];
     }
+
+    /** generate Pascal triangle
+    *Link: https://leetcode.com/problems/pascals-triangle/
+    *Time Complexity : O(n^2); space Complexity : O(n)
+    */
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(numRows < 0) return result;
+
+        for(int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<Integer>();
+            for(int j = 0; j < i + 1; j++) {
+                if(j == 0 || j == i)
+                row.add(1);
+                else {
+                row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+
+    /**
+    *Problem : generate pascal trinagle for given rows
+    *Link: https://leetcode.com/problems/pascals-triangle/
+    *Time Complexity : O(n)
+    */
+    public List<List<Integer>> generate(int numRows){
+	List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+	ArrayList<Integer> row = new ArrayList<Integer>();
+	for(int i=0;i<numRows;i++)
+	{
+		row.add(0, 1);
+		for(int j=1;j<row.size()-1;j++)
+			row.set(j, row.get(j)+row.get(j+1));
+		allrows.add(new ArrayList<Integer>(row));
+	}
+	return allrows;
+
+    }
     public static void main(String[] args) {
         MathSolution objMath = new MathSolution();
         System.out.println(objMath.fibonaciDP(8));
