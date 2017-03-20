dddd379735c65836d65478dc27f72a161b1cd343
vtiwari227 <srmistvipin@yahoo.co.in>
2017-03-19 23:23:15 -0700

    Code : find battleship in insland

--- Array/ArraySolution.java
+++ Array/ArraySolution.java
@@ -1241,7 +1241,37 @@ public class ArraySolution {
          }
       }
    }
-   
+
+   /**
+   *Problem : find battleship in board
+   *Link: https://leetcode.com/problems/battleships-in-a-board/
+   * Constraint:
+        1. Can board contain invalid battleship - No
+        2. Does board will be of finite size - Yes
+
+    Idea:
+        1.The key observation here is each battleship start when it have either
+        previous row and column empty
+   */
+   public int countBattleships(char[][] board) {
+        int m = board.length;
+        if (m==0) return 0;
+        int n = board[0].length;
+
+        int count=0;
+
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (board[i][j] == '.') continue;
+                if (i > 0 && board[i-1][j] == 'X') continue;
+                if (j > 0 && board[i][j-1] == 'X') continue;
+                count++;
+            }
+        }
+
+        return count;
+    }
+    
     public static void main(String[] args) {
         ArraySolution objArraySol = new ArraySolution();
         int input1[][] = {{1,1,1,0},
