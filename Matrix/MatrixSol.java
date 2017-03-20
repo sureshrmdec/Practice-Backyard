4d0ece42b48a1b675811f65377c5af138a37de9a
vtiwari227 <srmistvipin@yahoo.co.in>
2017-03-20 16:53:25 -0700

    Code : generate spiral matrix

--- Matrix/MatrixSol.java
+++ Matrix/MatrixSol.java
@@ -91,4 +91,59 @@ public class MatrixSol {
             }
         }
     }
+
+    /**
+    *Problem : Pint a given matrix in spiral format
+    *Link: https://leetcode.com/problems/spiral-matrix/
+    *Constraint:
+        1. Can matrix be empty
+        2. Does the matrix is square matrix
+    *Idea:
+        1. Travse the matrix first right,down,left and up and do this recurrsively
+        until you have processed all rows and column. Use two pointer rowBeign,colBegin
+        and rowEnd,ColEnd to increment and decreemnt pointers as you traverse array
+        Time Complexity : O(size of matrix) : since you are traversing one line
+        at a time either row or column
+    */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(matrix.length == 0) return result;
+
+        int rowBeign = 0;
+        int colBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colEnd = matrix[0].length - 1;
+
+        while(rowBegin <= rowEnd && colBegin <= colEnd) {
+            //traverse right
+            for(int i = colBegin; i<= colEnd; i++) {
+                result.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+
+            //traverse down
+            for(int i = rowBegin; i <= rowEnd; i++) {
+                result.add(matrix[rowEnd][i]);
+            }
+            colEnd--;
+
+
+        //traverse left
+            if(rowBegin <= rowEnd) {
+                for(int i = colEnd; i >= colBegin; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+
+            if(colBegin <= colEnd) {
+                for(int i = rowEnd; i >= rowBegin; i--) {
+                    result.add(matrix[i][colBegin]);
+                }
+            }
+            colBegin++;
+
+        }
+        return result;
+    }
 }
